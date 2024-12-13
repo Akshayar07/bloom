3f10579e2cd798ed066cbcd682feedc627238151
Akshayar07 <rakshaya22@gmail.com>
2024-12-14 02:08:46 +0530

    Docs: Appintialization file docs

--- lib/core/app_initializer.dart
+++ lib/core/app_initializer.dart
@@ -1,18 +1,25 @@
 import 'package:bloom/core/services/notification_service.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
+/// Utility class to handle all app level initialization
 class AppInitializer {
+  /// Main entry point for all initialization
   static Future<void> initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
     _initializeFirebase;
-    await _initializeNotification();
+    if (!kDebugMode) {
+      await _initializeNotification();
+    }
   }
 
+  ///Initializes firebase service
   static Future<void> _initializeFirebase() async {
     await Firebase.initializeApp();
   }
 
+  ///Initializes notification service
   static Future<void> _initializeNotification() async {
     await NotificationService.instance.initialize();
   }
